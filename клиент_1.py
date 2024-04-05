dc33ad2c26fce0126838419018b7bbe62aaf72ac
sofya <sofya.mixajlova.12@mail.ru>
2024-04-05 11:49:49 +0300

    Добавление файла .gitignore и исправление недочетов у первого клиента

--- клиент_1.py
+++ клиент_1.py
@@ -1,34 +1,26 @@
 import socket
 import json
 import os
-import socket
 import shutil
 
-# Создание TCP-клиента
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    #создание нового объекта сокета для работы с сетью
-                                                                     #первый аргумент socket.AF_INET указывает на используемый адресный семейство,
-                                                                     #в данном случае IPv4. Второй аргумент socket.SOCK_STREAM указывает на тип сокета,
-                                                                     #в данном случае потоковый сокет (TCP)
-server_address = ('127.0.0.1', 12353)    #привязка сокета к заданному хосту и порту
-
+server_address = ('127.0.0.1', 12360)    #привязка сокета к заданному хосту и порту
 print("Соединение с сервером")
-client_socket.connect(server_address)    #устанавливает соединение с удаленным сокетом, определенным по параметрам server_address
 
 def copy_path(src, dst): # три функции для выполнения "инструкций", полученных от сервера
     if os.path.isdir(src):
         shutil.copytree(src, dst)
-        print(f"Скопирована папка: {src} в {dst}")
+        return "Скопирована папка: " + str(src) + " в " + str(dst)
     else:
         shutil.copy2(src, dst)
-        print(f"Скопирован файл: {src} в {dst}")
+        return "Скопирован файл: " + str(src) + " в " + str(dst)
 
 def remove_path(path):
     if os.path.isdir(path):
         shutil.rmtree(path)
-        print(f"Удалена папка: {path}")
+        return "Удалена папка: " + str(path)
     else:
         os.remove(path)
-        print(f"Удален файл: {path}")
+        return "Удален файл: " + str(path)
 
 
 def translator(ans):
@@ -38,19 +30,22 @@ def translator(ans):
         for name in ans[1]:
             src_path = os.path.join(folder1, name)
             dest_path = os.path.join(folder2, name)
-            copy_path(src_path, dest_path)
+            s = copy_path(src_path, dest_path)
+            return s
     elif ans[0] == '2':
         for name in ans[1]:
             path = os.path.join(folder2, name)
-            remove_path(path)
+            s = remove_path(path)
+            return s
     else:
         for name in ans[1]:
             src_path = os.path.join(folder1, name)
             dest_path = os.path.join(folder2, name)
-            copy_path(src_path, dest_path)
+            s = copy_path(src_path, dest_path) + "\n"
         for name in ans[2]:
             path = os.path.join(folder2, name)
-            remove_path(path)
+            s += remove_path(path)
+        return s
 
 try:
     folder1 = os.mkdir("Папка 1")
@@ -60,36 +55,37 @@ except FileExistsError:
 try:
     folder2 = os.mkdir("Папка 2")
 except FileExistsError:
-        pass
+    pass
 
 folder1 = os.getcwd() + "\\Папка 1"
 folder2 = os.getcwd() + "\\Папка 2"
 
 
 while True:
-    action = int(input("Что вы хотите сделать? \n 1. Сравнить и прировнять содержимое папок \n 2. Разорвать соединение с сервером: "))
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #создание нового объекта сокета для работы с сетью
+                                                                       #первый аргумент socket.AF_INET указывает на используемый адресный семейство,
+                                                                       #в данном случае IPv4. Второй аргумент socket.SOCK_STREAM указывает на тип сокета,
+                                                                       #в данном случае потоковый сокет (TCP)
+    client_socket.connect(server_address)
+    action = input("Что вы хотите сделать? \n 1. Сравнить и прировнять содержимое папок \n 2. Разорвать соединение с сервером \nВвод: ")
 
-    if action == 1:
-        # Отправляем данные серверу
+    if action == "1":
+        client_socket.send("Сравнить и прировнять содержимое папок".encode())
         message1 = os.listdir(folder1)
         client_socket.send(json.dumps(message1).encode())
 
         message2 = os.listdir(folder2)
         client_socket.send(json.dumps(message2).encode())
 
-
         # Получаем ответ от сервера
         response = client_socket.recv(1024).decode()
         responce = json.loads(response)
         print(f"Ответ от сервера: {response}")
-
         print(translator(responce))
 
-    if action == 2:
-        # Отправляем данные серверу
-        message1 = 1
-        client_socket.send(json.dumps(message1).encode())
-
+    elif action == "2":
+        client_socket.close()
+        print("Разрыв соединения с сервером")
         s = input("Хотите ли вы подключиться к серверу снова? Напишите да/нет: ")
         if s == "да":
             client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    #создается новый объект клиентского сокета
@@ -102,3 +98,5 @@ while True:
             print("Вы ввели некорректные данные!")
             client_socket.close()
             break
+    else:
+        print("Вы ввели некорректное значение! Попробуйте заново!")
